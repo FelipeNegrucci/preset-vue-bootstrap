b1f3c9e96c4526e5d201a5429014f3f14f91d178
FelipeNegrucci <felipe.negrucci@protonmail.com>
2024-07-16 08:47:54 -0300

    feat: Add ability to modify Bootstrap color modes
    - Implemented functionality to switch between different color modes
    - Updated the styles to support dynamic color changes
    - Stored the selected color mode in localStorage for persistence

--- frontend/src/views/mainView.vue
+++ frontend/src/views/mainView.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { useBootstrapColorMode } from '@/composables/useBootstrapColorMode';
 import myComp from '../components/myComp.vue'
+
+const { toggleBootstrapColorMode } = useBootstrapColorMode();
 </script>
 
 <template>
-    <myComp />
+    <div class="container-fluid user-select-none">
+        <div class="row row-cols-1 justify-content-center gap-3">
+        
+            <div @click="toggleBootstrapColorMode()" class="col-auto btn btn-primary mt-3">Toggle Bootstrap ColorMode</div>
+            <myComp class="col" />
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss"></style>
