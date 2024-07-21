00b0b171d8d6544946a6ac7ba1240fe69264b3ec
FelipeNegrucci <felipe.negrucci@protonmail.com>
2024-07-21 18:41:15 -0300

    refactor: Reorganize Bootstrap SCSS files for improved structure
    - Refactored SCSS files to enhance maintainability and readability
    - Reorganized SCSS configurations and variables for better structure

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,25 +1,9 @@
+import('@/assets/bootstrap/bootstrap.scss');
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const bootstrapOption = 1
-switch (bootstrapOption) {
-    case 1: {
-        import('@/assets/bootstrap01.scss');
-        break;
-    }
-    case 2: {
-        import('@/assets/bootstrap02.scss');
-        break;
-    }
-    default: {
-        import('@/assets/bootstrapDefault.scss');
-        break;
-    }
-}
-
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
